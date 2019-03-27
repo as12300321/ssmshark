0af728e2f97b97fbc20c5c1300b0b2809ca2d761
Sazz <sf2033@163.com>
2019-03-28 01:09:29 +0800

    【SLF0004】frist add spring boot start

--- src/main/java/com/shark/dao/User_vipMapper.java
+++ src/main/java/com/shark/dao/User_vipMapper.java
@@ -17,6 +17,7 @@ public interface User_vipMapper {
 
     @Select(" SELECT * FROM user_vip ")
     List<user_vip> SelectAll();
+
     
      @Insert("insert into user_vip values(#{id},#{user_name},#{password},#{phone},#{gender},#{target},#{action},#{insert_ts},#{other},#{email},#{update_ts},#{head_protrait})")
     int insertUser_vip(user_vip  user_vip);
