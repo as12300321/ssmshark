9af67ffa09d8a5444820e6e4e02bd24238b1fcf4
279658607@qq.com <279658607@qq.com>
2019-03-31 23:52:57 +0800

    meas

--- src/main/java/com/shark/dao/User_vipMapper.java
+++ src/main/java/com/shark/dao/User_vipMapper.java
@@ -16,11 +16,12 @@ public interface User_vipMapper {
     @Select(" SELECT * FROM user_vip  where id= #{id} ")
     User Sel(int id);
 
+    @Select("select * from user_vip where user_name=#{user_name}")
+    int  selectByUsername(String user_name);
     @Select(" SELECT * FROM user_vip ")
     List<user_vip> SelectAll();
-    @Update("")
-    int update( );
-     @Insert("insert into user_vip values(#{id},#{user_name},#{password},#{phone},#{gender},#{target},#{action},#{insert_ts},#{other},#{email},#{update_ts},#{head_protrait})")
+    @Update("update   user_vip  set user_name=#{user_name} where id=#{id} ")
+    int update(int id,user_vip  user_vip );
+    @Insert("insert into user_vip values(#{id},#{user_name},#{password},#{phone},#{gender},#{target},#{action},#{insert_ts},#{other},#{email},#{update_ts},#{head_protrait})")
     int insertUser_vip(user_vip  user_vip);
-
 }
